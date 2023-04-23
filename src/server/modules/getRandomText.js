86234bdf8be9c9b90040806f83561886d86b1a00
Владимир Говоров <govorov2001gv@gmail.com>
2023-04-23 08:14:58 +0300

    remove 1 secret

--- src/server/modules/getRandomText.js
+++ src/server/modules/getRandomText.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 async function getRandomText(numberSequences = 2) {
     const response = await fetch(`https://api.api-ninjas.com/v1/loremipsum`, {
         headers: {
-            'X-Api-Key': 'Wu3RPXRr41apE4FFnXAl9w==iloLIsliV47rK6tO'
+            'X-Api-Key': ''
         }
     });
 
